67e861f08c7c5d14d4d41ceff59841594cb7f11a
SlowCDW <1quick1@list.ru>
2020-12-17 20:50:58 +0300

    complete verst

--- app/src/main/java/com/slowcdw/myapplication/FragmentMoviesList.kt
+++ app/src/main/java/com/slowcdw/myapplication/FragmentMoviesList.kt
@@ -1,17 +1,24 @@
 package com.slowcdw.myapplication
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesList : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
-        val cardView = view.findViewById<CardView>(R.id.cardView);
-        cardView.setBackgroundResource(R.drawable.img_rount_top_corners);
+        val img= view.findViewById<ImageView>(R.id.preview_img_1);
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.movie_list_1);
+        val dr = RoundedBitmapDrawableFactory.create(resources, bitmap);
+        dr.cornerRadius = 8F;
+        img.setImageDrawable(dr);
         return view
     }
 }
